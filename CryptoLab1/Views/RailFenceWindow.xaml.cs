33ecc6ae0e396e41a8dd8f0a1435f25aa6d9bf38
misxka <mishutkavlesu@gmail.com>
2022-02-05 11:36:02 +0300

    feat: implement railFence decryption

--- CryptoLab1/Views/RailFenceWindow.xaml.cs
+++ CryptoLab1/Views/RailFenceWindow.xaml.cs
@@ -22,12 +22,18 @@ namespace CryptoLab1.Views
     {
         private int key;
         private String phrase;
-        private String encryptedPhrase;
+        private String resultedPhrase;
 
         private enum Direction
         {
             Rise,
             Decrease
+        }
+
+        private enum Option
+        {
+            Encrypt,
+            Decrypt
         }
 
         public RailFenceWindow()
@@ -36,7 +42,7 @@ namespace CryptoLab1.Views
 
             this.key = 1;
             this.phrase = "";
-            this.encryptedPhrase = "";
+            this.resultedPhrase = "";
         }
 
         private void HandlePreviewTextInput(object sender, TextCompositionEventArgs e)
@@ -57,9 +63,29 @@ namespace CryptoLab1.Views
                 key = Int32.Parse(keyInput.Text);
                 phrase = phraseInput.Text;
 
-                (encryptedPhrase, char[,] arr) = encrypt(phrase, key);
+                (resultedPhrase, char[,] arr) = encrypt(phrase, key);
 
-                resultPhrase.Text = encryptedPhrase;
+                resultPhrase.Text = resultedPhrase;
+                resultGrid.Visibility = Visibility.Visible;
+
+                viewMatrix(arr);
+            }
+            catch (FormatException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private void DecryptPhrase(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                key = Int32.Parse(keyInput.Text);
+                phrase = phraseInput.Text;
+
+                (resultedPhrase, char[,] arr) = decrypt(phrase, key);
+
+                resultPhrase.Text = resultedPhrase;
                 resultGrid.Visibility = Visibility.Visible;
 
                 viewMatrix(arr);
@@ -90,51 +116,87 @@ namespace CryptoLab1.Views
 
         private (string, char[,]) encrypt(string phrase, int key)
         {
-            char[,] arr = fillEncMatrix(phrase, key);
+            char[,] arr = fillMatrix(phrase, key, Option.Encrypt);
 
             return (getEncryptedString(arr), arr);
         }
 
-        private char[,] fillEncMatrix(string phrase, int key)
+        private (string, char[,]) decrypt(string phrase, int key)
+        {
+            char[,] arr = fillMatrix(phrase, key, Option.Decrypt);
+
+            return (getDecryptedString(arr), arr);
+        }
+
+        private char[,] fillMatrix(string phrase, int key, Option option)
         {
             int phraseIndex = 0;
-            int colIndex = 0;
             Direction direction = Direction.Rise;
 
-            char[,] arr = fillMatrix(new char[key, phrase.Length]);
+            char[,] arr = fillMatrixWithNull(new char[key, phrase.Length]);
 
-            int phraseLength = phrase.Length;
-            while (phraseIndex < phraseLength)
+            if (option == Option.Encrypt) fillEncMatrix(phrase, key);
+            else fillDecryptMatrix(phrase, key);
+
+
+            void fillEncMatrix(string phrase, int key)
             {
-                arr[colIndex, phraseIndex] = phrase[phraseIndex++];
+                int rowIndex = 0;
+                int phraseLength = phrase.Length;
 
-                colIndex = iterateColIndex(direction, colIndex);
-                if (colIndex == 0 || colIndex == key - 1) direction = changeDirection();
+                while (phraseIndex < phraseLength)
+                {
+                    arr[rowIndex, phraseIndex] = phrase[phraseIndex++];
+
+                    rowIndex = iterateRowIndex(direction, rowIndex);
+                    if (rowIndex == 0 || rowIndex == key - 1) direction = changeDirection(ref direction);
+                }
             }
 
-            char[,] fillMatrix(char[,] arr)
+            void fillDecryptMatrix(string phrase, int key)
             {
-                for (int i = 0; i < arr.GetLength(0); i++)
+                int phraseLength = phrase.Length;
+                for(int row = 0; row < key; row++)
                 {
-                    for (int j = 0; j < arr.GetLength(1); j++)
+                    int col = row;
+                    bool reverseFlag = false;
+                    while(col < phraseLength)
                     {
-                        arr[i, j] = '\0';
+                        arr[row, col] = phrase[phraseIndex++];
+
+                        if (row == 0) col += (key - row - 1) * 2;
+                        else if (row == key - 1) col += (key - (key - row)) * 2;
+                        else if (!reverseFlag) col += (key - row - 1) * 2;
+                        else col += (key - (key - row)) * 2;
+
+                        reverseFlag = !reverseFlag;
                     }
                 }
-                return arr;
-            }
-
-            Direction changeDirection()
-            {
-                return (direction == Direction.Rise) ? Direction.Decrease : Direction.Rise;
-            }
-
-            int iterateColIndex(Direction direction, int colIndex)
-            {
-                return (direction == Direction.Rise) ? ++colIndex : --colIndex;
             }
 
             return arr;
+        }
+
+        private char[,] fillMatrixWithNull(char[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = '\0';
+                }
+            }
+            return arr;
+        }
+
+        private Direction changeDirection(ref Direction direction)
+        {
+            return (direction == Direction.Rise) ? Direction.Decrease : Direction.Rise;
+        }
+
+        private int iterateRowIndex(Direction direction, int rowIndex)
+        {
+            return (direction == Direction.Rise) ? ++rowIndex : --rowIndex;
         }
 
         private string getEncryptedString(char[,] arr)
@@ -144,6 +206,26 @@ namespace CryptoLab1.Views
             foreach (char c in arr)
             {
                 if(c != '\0') stringBuilder.Append(c);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private string getDecryptedString(char[,] arr)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            Direction direction = Direction.Rise;
+            int rowIndex = 0;
+
+            for(int colIndex = 0; colIndex < arr.GetLength(1); colIndex++) {
+                if (arr[rowIndex, colIndex] != '\0')
+                {
+                    stringBuilder.Append(arr[rowIndex, colIndex]);
+                    rowIndex = iterateRowIndex(direction, rowIndex);
+                }
+
+                if (rowIndex == 0 || rowIndex == key - 1) direction = changeDirection(ref direction);
             }
 
             return stringBuilder.ToString();
